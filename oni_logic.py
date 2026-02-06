171408e43a602c01d06081f3de97a4f9080de4ba
Badro-NinjaDev <tdy48z9w6p@privaterelay.appleid.com>
2026-02-06 16:04:49 +0100

    Create oni_logic.py

---
+++ oni_logic.py
@@ -0,0 +1,8 @@
+print("Oni AI System: Online")
+print("Welcome back, Lead Engineer Badro")
+
+command = input("سيدي، هل تود تفعيل وضع النينجا؟ (نعم/لا): ")
+if command == "نعم":
+    print("🥷 وضع Oni-Fury نشط الآن!")
+else:
+    print("💤 الأنظمة في وضع الاستعداد...")
